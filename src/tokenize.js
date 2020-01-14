56014f10f96fe6b9ed20bc63b1472690bd2f91b9
sandeep chahal <sandeepchahal573@gmail.com>
2020-01-14 10:08:18 +0530

    first commit

---
+++ src/tokenize.js
@@ -0,0 +1,14 @@
+const {
+  isLetter,
+  isWhitespace,
+  isNumber,
+  isParenthesis,
+  isQuote,
+} = require('./identify');
+
+const tokenize = input => {
+  const tokens = [];
+  let cursor = 0;
+};
+
+module.exports = { tokenize };
