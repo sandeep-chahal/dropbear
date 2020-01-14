e9a10e260fa557b2e1a8ab140f1cd881309ae1ff
sandeep chahal <sandeepchahal573@gmail.com>
2020-01-14 10:52:16 +0530

    completed exercise 1

--- src/tokenize.js
+++ src/tokenize.js
@@ -9,6 +9,31 @@ const {
 const tokenize = input => {
   const tokens = [];
   let cursor = 0;
+
+  while (cursor < input.length) {
+    const character = input[cursor];
+
+    if (isParenthesis(character)) {
+      tokens.push({ type: 'Parenthesis', value: character });
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace(character)) {
+      cursor++;
+      continue;
+    }
+
+    if (isNumber(character)) {
+      tokens.push({ type: 'Number', value: Number(character) });
+      cursor++;
+    }
+    if (isLetter(character)) {
+      tokens.push({ type: 'Name', value: character });
+      cursor++;
+    }
+  }
+  return tokens;
 };
 
 module.exports = { tokenize };
--- tests/tokenize.test.js
+++ tests/tokenize.test.js
@@ -1,11 +1,11 @@
 import { tokenize } from '../src/tokenize';
 
 describe(tokenize, () => {
-  it.skip('should return an array', () => {
+  it('should return an array', () => {
     expect(Array.isArray(tokenize(''))).toBe(true);
   });
 
-  it.skip('should be able to tokenize a pair of parentheses', () => {
+  it('should be able to tokenize a pair of parentheses', () => {
     const input = '()';
     const result = [
       { type: 'Parenthesis', value: '(' },
@@ -15,7 +15,7 @@ describe(tokenize, () => {
     expect(tokenize(input)).toEqual(result);
   });
 
-  it.skip('should ignore whitespace completely', () => {
+  it('should ignore whitespace completely', () => {
     const input = '                  ';
     const result = [];
 
@@ -23,14 +23,14 @@ describe(tokenize, () => {
   });
 
   // Exercise 1 - Begin
-  it.skip('should correctly tokenize a single digit', () => {
+  it('should correctly tokenize a single digit', () => {
     const input = '2';
     const result = [{ type: 'Number', value: 2 }];
 
     expect(tokenize(input)).toEqual(result);
   });
 
-  it.skip('should be able to handle single numbers in expressions', () => {
+  it('should be able to handle single numbers in expressions', () => {
     const input = '(1 2)';
 
     const result = [
@@ -43,7 +43,7 @@ describe(tokenize, () => {
     expect(tokenize(input)).toEqual(result);
   });
 
-  it.skip('should be able to handle single letters in expressions', () => {
+  it('should be able to handle single letters in expressions', () => {
     const input = '(a b)';
 
     const result = [
