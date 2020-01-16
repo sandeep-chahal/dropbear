0b9224071955086eac15e86ce2c1a5cb4413c68b
sandeep chahal <sandeepchahal573@gmail.com>
2020-01-16 17:35:29 +0530

    implemented traverseNode function

--- src/traverse.js
+++ src/traverse.js
@@ -1,3 +1,26 @@
+const traverseNode = ({ node, parent, visitor }) => {
+  const methods = visitor[node.type];
+
+  if (methods && methods.enter) {
+    methods.enter({ node, parent });
+  }
+
+  if (node.arguments) {
+    traverseArray({ array: node.arguments, parent: node, visitor });
+  }
+
+  if (methods && methods.exit) {
+    methods.exit({ node, parent });
+  }
+};
+
 const traverse = (node, visitor) => {};
 
 module.exports = { traverse };
+
+const visitor = {
+  VariableDeclaration: {
+    enter({ node, parent }) {},
+    exit({}) {},
+  },
+};
